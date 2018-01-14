1b1903ca3557adaeba54e0d5eb79ea69de34bb7a
Korrawit Chanthong <witrakor@korrawits-macbook-air.local>
2018-01-14 17:22:54 +0700

    add tableview and configure cell with checkmark

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Korrawit Chanthong on 14/1/2561 BE.
+//  Copyright © 2561 Witrakor. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike","Buy Eggos", "Destory Demogorgon"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
